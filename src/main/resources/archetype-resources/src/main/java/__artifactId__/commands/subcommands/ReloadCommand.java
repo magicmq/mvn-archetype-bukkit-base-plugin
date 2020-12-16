a37717e97bd30595cb93d39b750b7c1ce6b9b3e5
magicmq <mquindt@yahoo.com>
2020-12-15 18:53:59 -0600

    Add confirmation message when reload command is executed

--- src/main/resources/archetype-resources/src/main/java/__artifactId__/commands/subcommands/ReloadCommand.java
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__/commands/subcommands/ReloadCommand.java
@@ -3,6 +3,7 @@ package ${groupId}.${artifactId}.commands.subcommands;
 import ${groupId}.${artifactId}.${name};
 import ${groupId}.${artifactId}.commands.SubCommand;
 import ${groupId}.${artifactId}.commands.SubCommandMeta;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 @SubCommandMeta(
@@ -14,6 +15,7 @@ public class ReloadCommand implements SubCommand {
     @Override
     public boolean onCommand(CommandSender sender, String[] args) {
         ${name}.get().reload();
+        sender.sendMessage(ChatColor.GREEN + "Configuration has been reloaded.");
         return true;
     }
 }
